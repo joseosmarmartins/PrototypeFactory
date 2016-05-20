9ba9b7ccf6978e30b2f0b2233b3b03bc46c2f259
José Osmar Martins Silva <joseosmarmartins@hotmail.com>
2016-05-20 19:46:33 -0300

    alterações multiplas classes.

--- src/com/jose/Main.java
+++ src/com/jose/Main.java
@@ -3,14 +3,14 @@ package com.jose;
 public class Main {
 
     public static void main(String[] args) {
-	    PrototypeFactory fabricaAula = new PrototypeFactory(new AulaNormal("Prototype Factory"));
+	    PrototypeFactory fabricaAula = new PrototypeFactory(new AulaProva("prova de Abstract Factory"));
 
         Aula aula = fabricaAula.criarClone();
         System.out.println("Conteudo da aula: " + aula.getConteudoAula());
 
         System.out.println("\n");
 
-        PrototypeFactory fabricaProva = new PrototypeFactory(new AulaNormal("prova de Prototype Factory"));
+        PrototypeFactory fabricaProva = new PrototypeFactory(new AulaProva("prova de Prototype Factory"));
 
         Aula prova = fabricaProva.criarClone();
         System.out.println("Conteudo da aula: " + prova.getConteudoAula());
